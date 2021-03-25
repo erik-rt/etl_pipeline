b2d82b1bad831615a2686abf3fe78cc84afd54e7
erik-tastepe <erik.tastepe@gmail.com>
2021-03-25 17:28:14 -0400

    cleaned up etl file

--- src/etl.py
+++ src/etl.py
@@ -1,40 +1,40 @@
 import os
 import psycopg2
-import yaml
 
+from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from utils.redshift_tables import make_tables
+
 
 class ETLPipeline():
     def __init__(self):
         pass
 
     def initiate_spark(self):
-        spark = SparkSession.builder.appName()
-    def connect_to_redshift(self, connection):
+        spark = (
+            SparkSession.builder.appName('ETL Pipeline')
+            .builder.config(conf=SparkConf())
+            .getOrCreate()
+            )
+        return spark
+
+    def connect_to_redshift(self):
         connection = psycopg2.connect(
-            f'host={os.environ['REDSHIFT_HOST']}'
-            f'dbname={os.environ['REDSHIFT_DBNAME']}'
-            f'user={os.environ['REDSHIFT_USER']}'
-            f'password={os.environ['REDSHIFT_PW']}'
-            f'port={os.environ['REDSHIFT_PORT']}'
+            f'host={os.environ["REDSHIFT_HOST"]}'
+            f'dbname={os.environ["REDSHIFT_DBNAME"]}'
+            f'user={os.environ["REDSHIFT_USER"]}'
+            f'password={os.environ["REDSHIFT_PW"]}'
+            f'port={os.environ["REDSHIFT_PORT"]}'
             )
         cursor = connection.cursor()
+        return connection, cursor
 
-    def main():
-        connection = psycopg2.connect(
-            f'host={os.environ['REDSHIFT_HOST']}'
-            f'dbname={os.environ['REDSHIFT_DBNAME']}'
-            f'user={os.environ['REDSHIFT_USER']}'
-            f'password={os.environ['REDSHIFT_PW']}'
-            f'port={os.environ['REDSHIFT_PORT']}'
-            )
-        cursor = connection.cursor()
-        cfg = 'config.yaml'
+    def run(self):
+        connection, cursor = self.connect_to_redshift()
+        make_tables(connection, cursor, config='config.yaml')
 
-        make_tables(connection, cursor, cfg)
-        
         connection.close()
 
+
 if __name__ == '__main__':
-    main()
+    ETLPipeline.run()
