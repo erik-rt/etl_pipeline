1de723db3daadc5c4e025810bd107f636f880157
erik-tastepe <erik.tastepe@gmail.com>
2021-03-27 11:51:00 -0400

    added local data

--- src/etl.py
+++ src/etl.py
@@ -14,6 +14,7 @@ class ETLPipeline():
         spark = (
             SparkSession.builder.appName('ETL Pipeline')
             .builder.config(conf=SparkConf())
+            .enableHiveSupport()
             .getOrCreate()
             )
         return spark
